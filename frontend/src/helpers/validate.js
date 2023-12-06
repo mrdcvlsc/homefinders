981a2100751f41a195a4ffad38bacbd2a3011d2a
mrdcvlsc <mrdcvlsc@gmail.com>
2023-12-06 10:40:54 +0800

    updated username regex in frontend

--- frontend/src/helpers/validate.js
+++ frontend/src/helpers/validate.js
@@ -1,5 +1,5 @@
 const EMAIL_REGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-const USERNAME_REGEX = /^[a-zA-Z0-9]+$/
+const USERNAME_REGEX = /^[a-zA-Z0-9!#$%^&*_.+-]+$/
 
 // Password should contain at least 1 lowercase and uppercase letter each, 1 number and 1 special character
 const PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/
@@ -14,9 +14,9 @@ export function validateForm(username, password, retyped_password = null) {
     } else if (username.includes('@') && username.length > 254) {
         form_errors.username = "*RFC 2821 violation, maximum email characters is 254"
     } else if (username.includes('@') && !EMAIL_REGEX.test(username)) {
-        form_errors.username = "*Invalid email, contains invalid characters"
+        form_errors.username = "*Invalid email format or might contain invalid characters"
     } else if (!username.includes('@') && !USERNAME_REGEX.test(username)) {
-        form_errors.username = "*Invalid username, contains invalid characters"
+        form_errors.username = "*Only alphanumeric characters and !#$%^&*_.+- are allowed"
     } else if (!username.includes('@') && username.length > 25) {
         form_errors.username = "*Invalid username, maximum username characters is 25"
     }
