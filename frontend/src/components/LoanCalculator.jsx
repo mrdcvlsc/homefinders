11966f83f5912f46d97ce4584721737a43651bc7
mrdcvlsc <mrdcvlsc@gmail.com>
2023-12-12 22:16:28 +0800

    working loan calculator

--- frontend/src/components/LoanCalculator.jsx
+++ frontend/src/components/LoanCalculator.jsx
@@ -1,6 +1,36 @@
+import React from 'react';
 import '../styles/calculator.css';
 
 export default function LoanCalculator() {
+    const loanAmount = React.useRef(0)
+    const interestRate = React.useRef(0)
+    const loanTenure = React.useRef(0)
+
+    const [loanEMI, setLoanEMI] = React.useState(0)
+    const [totalInterestPayable, setTotalInterestPayable] = React.useState(0)
+    const [totalAmount, setTotalAmount] = React.useState(0)
+
+    const calculateBtnHandler = () => {
+        const interest = interestRate.current / 12 / 100;
+
+        // calculate EMI
+        const newLoanEMI = 
+            loanAmount.current * interest *
+            (Math.pow(1 + interest, loanTenure.current) /
+            (Math.pow(1 + interest, loanTenure.current) - 1 ))
+    
+        const newTotalAmount = Math.round(loanTenure.current * newLoanEMI)
+        const newTotalInterestPayable = Math.round(newTotalAmount - loanAmount.current)
+
+        // a note to my future self : mf only render states after all the
+        // calculations are done, it will save you 2 hours of debugging time,
+        // useState calls are asynchronous so you can not trust then in
+        // between sequential calculations.
+        setLoanEMI(Math.round(newLoanEMI))
+        setTotalAmount(newTotalAmount)
+        setTotalInterestPayable(newTotalInterestPayable)
+    }
+
     return (
         <div className="loan-calculator-form">
             <div className="loan-calculator-top">
@@ -9,17 +39,17 @@ export default function LoanCalculator() {
                 <form action="#">
                     <div>
                         <div className="loan-calculator-labels">Amount</div>
-                        <input type="text" value="0"/>
+                        <input type="number" onChange={(e) => loanAmount.current = e.target.value}/>
                     </div>
 
                     <div>
                         <div className="loan-calculator-labels">Interest Rate</div>
-                        <input type="text" value="0"/>
+                        <input type="number" onChange={(e) => interestRate.current = e.target.value}/>
                     </div>
 
                     <div>
                         <div className="loan-calculator-labels">Tenure (in months)</div>
-                        <input type="text" value="0"/>
+                        <input type="number" onChange={(e) => loanTenure.current = e.target.value}/>
                     </div>
                 </form>
             </div>
@@ -29,20 +59,20 @@ export default function LoanCalculator() {
                 <div className="loan-calcu-result-left">  
                     <div>
                         <h3>Loan EMI</h3>
-                        <div className="loan-calcu-output-val">123</div>
+                        <div className="loan-calcu-output-val">{loanEMI}</div>
                     </div>
 
                     <div>
                         <h3>Total Interest Payable</h3>
-                        <div className="loan-calcu-output-val">123</div>
+                        <div className="loan-calcu-output-val">{totalInterestPayable}</div>
                     </div>
 
                     <div>
                         <h3>Total Amount</h3>
-                        <div className="loan-calcu-output-val">123</div>
+                        <div className="loan-calcu-output-val">{totalAmount}</div>
                     </div>
 
-                    <button className="loan-calculate-btn">Calculate</button>
+                    <button className="loan-calculate-btn" onClick={() => calculateBtnHandler()}>Calculate</button>
                 </div>
 
                 {/* <div className="right">
