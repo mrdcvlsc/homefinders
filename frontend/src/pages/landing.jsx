021b3035ddbbe45dd2b41a3af8a58ec8dbbf5964
mrdcvlsc <mrdcvlsc@gmail.com>
2024-01-12 15:16:03 +0800

    implement landing page raw layout

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -4,15 +4,43 @@ import "../styles/landing.css";
 export default function Landing() {
   return (
     <div id="landing-page">
-      <h1>Landing Page</h1>
-      <br />
-      <h3>Home Finders - Landing Page</h3>
-      <br />
-      <h3>
-        <i>coming soon...</i>
-      </h3>
-      <br />
-      {/* <UploadDragOrSelect /> */}
+      <h1>Discover Your Perfect Home</h1>
+      <h3>Your Journey Starts Here</h3>
+      
+      <div className="search-box">
+        <div className="drop-box-container">
+          <p>Choose preferred province</p>
+          <select onChange={ e => console.log(e.target.value) }>
+            <option key={'default'} value={''} label="N/A"/>
+            <option key={'Cavite'} value={'Cavite'} label="Cavite"/>
+            <option key={'Batangas'} value={'Batangas'} label="Batangas"/>
+          </select>
+        </div>
+
+        <div className="drop-box-container">
+          <p>Choose property type</p>
+          <select onChange={ e => console.log(e.target.value) }>
+            <option key={'default'} value={''} label="N/A"/>
+            <option key={'Residential'} value={'Residential'} label="Residential"/>
+            <option key={'Semi-Detached'} value={'Semi-Detached'} label="Semi-Detached"/>
+          </select>
+        </div>
+
+        <div className="drop-box-container">
+          <p>Choose preferred price range</p>
+          <select onChange={ e => console.log(e.target.value) }>
+            <option key={'default'} value={''} label="N/A"/>
+            <option key={'1'} value={'1'} label="1,000,000 - 3,000,000"/>
+            <option key={'2'} value={'2'} label="3,000,000 - 5,000,000"/>
+            <option key={'3'} value={'3'} label="5,000,000 - 10,000,000"/>
+            <option key={'4'} value={'4'} label="10,000,000 or Above"/>
+          </select>
+        </div>
+
+        <div className="drop-box-container">
+          <button>SEARCH</button>
+        </div>
+      </div>
     </div>
   );
 }
